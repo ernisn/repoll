aac4e32c054cfae78c7c72c20bcda4df7c2ed755
ernie <esu@live.com>
2022-06-11 19:43:53 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  padding:0.4em;
+  font-weight: bold;
+  color: blue;
+}
+
+#nav a.router-link-exact-active {
+  background-color: blue;
+  color: white;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Start from '../views/Start.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Start',
+    component: Start
+  },
+    {
+    path: '/poll/:id',
+    name: 'Poll',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Poll.vue')
+  },
+  {
+    path: '/create/:lang',
+    name: 'Create',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Create.vue')
+  },
+  {
+    path: '/result/:id',
+    name: 'Result',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Result.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
