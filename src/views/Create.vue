5f1895ebbf6f4789404e02c65b4ea8e6ee1f9a35
Ernie <esu@live.com>
2022-06-13 18:19:53 +0200

    Initial page

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,4 +1,18 @@
 <template>
+  <header>
+    <div v-bind:class="['hamburger', {'close': !hideNav}]"
+         v-on:click="toggleNav">
+    </div>
+    <div class="logo"><img src="/img/logo.png">rePoll</div>
+  </header>
+
+  <ResponsiveNav v-bind:hideNav="hideNav">
+    <router-link v-bind:to="'/'">Join Poll</router-link>
+    <router-link v-bind:to="'/create/'+lang">{{uiLabels.createPoll}}</router-link>
+    <router-link v-bind:to="'/result/'+id">Poll Results</router-link>
+    <button v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
+  </ResponsiveNav>
+
   <div>
     Poll link: 
     <input type="text" v-model="pollId">
@@ -31,11 +45,15 @@
 </template>
 
 <script>
+import ResponsiveNav from '@/components/ResponsiveNav.vue';
 import io from 'socket.io-client';
 const socket = io();
 
 export default {
   name: 'Create',
+  components: {
+    ResponsiveNav
+  },
   data: function () {
     return {
       lang: "",
@@ -75,3 +93,55 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+header {
+  background-color: gray;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 2em auto;
+}
+.logo {
+  text-transform: uppercase;
+  letter-spacing: 0.25em;
+  font-size: 2.5rem;
+  color: white;
+  padding-top:0.2em;
+}
+.logo img {
+  height:2.5rem;
+  vertical-align: bottom;
+  margin-right: 0.5rem;
+}
+.hamburger {
+  color:white;
+  width:1em;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  padding:0.5rem;
+  top:0;
+  left:0;
+  height: 2rem;
+  cursor: pointer;
+  font-size: 1.5rem;
+}
+
+@media screen and (max-width:50em) {
+  .logo {
+    font-size: 5vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .hamburger::before {
+    content: "☰";
+  }
+  .close::before {
+    content: "✕";
+  }
+  .hide {
+    left:-12em;
+  }
+}
+</style>
--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,26 +1,41 @@
 <template>
-  <div id="nav">
-    <button v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
+  <header>
+    <div v-bind:class="['hamburger', {'close': !hideNav}]" 
+         v-on:click="toggleNav">
+    </div>
+    <div class="logo"><img src="/img/logo.png">rePoll</div>
+  </header>
+
+  <ResponsiveNav v-bind:hideNav="hideNav">
+    <router-link v-bind:to="'/'">Join Poll</router-link>
     <router-link v-bind:to="'/create/'+lang">{{uiLabels.createPoll}}</router-link>
-    <label>
-      Write poll id: 
-      <input type="text" v-model="id">
-    </label>
-    <router-link v-bind:to="'/poll/'+id" tag="button">{{uiLabels.participatePoll}}</router-link>
-  </div>
+    <router-link v-bind:to="'/result/'+id">Poll Results</router-link>
+    <button v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
+  </ResponsiveNav>
+
+  <label>
+    Write poll id: 
+    <input type="text" v-model="id">
+  </label>
+  <router-link v-bind:to="'/poll/'+id" tag="button">{{uiLabels.participatePoll}}</router-link>
 </template>
 
 <script>
+import ResponsiveNav from '@/components/ResponsiveNav.vue';
 import io from 'socket.io-client';
 const socket = io();
 
 export default {
   name: 'Start',
+  components: {
+    ResponsiveNav
+  },
   data: function () {
     return {
       uiLabels: {},
       id: "",
-      lang: "en"
+      lang: "en",
+      hideNav: true
     }
   },
   created: function () {
@@ -35,7 +50,62 @@ export default {
       else
         this.lang = "en"
       socket.emit("switchLanguage", this.lang)
+    },
+    toggleNav: function () {
+      this.hideNav = ! this.hideNav;
     }
   }
 }
 </script>
+
+<style scoped>
+  header {
+    background-color: gray;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 2em auto;
+  }
+  .logo {
+    text-transform: uppercase;
+    letter-spacing: 0.25em;
+    font-size: 2.5rem;
+    color: white;
+    padding-top:0.2em;
+  }
+  .logo img {
+    height:2.5rem;
+    vertical-align: bottom;
+    margin-right: 0.5rem; 
+  }
+  .hamburger {
+    color:white;
+    width:1em;
+    display: flex;
+    align-items: center;
+    justify-content: left;
+    padding:0.5rem;
+    top:0;
+    left:0;
+    height: 2rem;
+    cursor: pointer;
+    font-size: 1.5rem;
+  }
+
+@media screen and (max-width:50em) {
+  .logo {
+    font-size: 5vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .hamburger::before {
+    content: "☰";
+  }
+  .close::before {
+    content: "✕";
+  }
+  .hide {
+    left:-12em;
+  }
+}
+</style>
